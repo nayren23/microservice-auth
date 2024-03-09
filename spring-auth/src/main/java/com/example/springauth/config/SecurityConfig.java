fede5a152042100961e015709a594502793ad7be
Nayren <rchouchane@iut.univ-paris8.fr>
2024-03-09 20:56:45 +0100

    feat: ajout des cors

--- spring-auth/src/main/java/com/example/springauth/config/SecurityConfig.java
+++ spring-auth/src/main/java/com/example/springauth/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +20,15 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 );
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/auth/**").allowedOrigins("http://localhost:4200");
+            }
+        };
     }
 }
